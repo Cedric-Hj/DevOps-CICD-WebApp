50b89a7cc5da220fc62a4aeae1e57f6b3468ee9d
Cedric-HJ <Devops-cedric.hj@gmail.com>
2024-05-26 11:17:04 +0100

    Terraform file config update for VPC + subnet

--- Terraform/Terraform playbook V2.tf
+++ Terraform/Terraform playbook V2.tf
@@ -1,3 +1,12 @@
+
+
+#############################################################
+#                                                           #
+#                       VPC creation                        #
+#                                                           #
+#############################################################
+
+
 # Define the AWS provider and specify the region
 provider "aws" {
   region = "us-east-1" # Specify your desired region
@@ -13,6 +22,7 @@ data "http" "my_ip" {
 resource "aws_security_group" "jenkins_sg" {
   name        = "jenkins_sg"                # Name of the security group
   description = "Security group for Jenkins server allowing SSH and HTTP access" # Description of the security group
+  vpc_id = aws_vpc.DCHJ_VPC_Fullstack_CICD.id
 
   # Ingress rule to allow SSH access (port 22) from your IP address
   ingress {
@@ -66,6 +76,10 @@ resource "aws_instance" "jenkins_server" {
   }
 
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id] # Attach the security group to the instance
+  
+  subnet_id = aws_subnet.DCHJ_Public_Subnet_01.id
+
+
 }
 
 # Output the public IP address of the EC2 instance
@@ -73,3 +87,111 @@ output "instance_public_ip" {
   description = "The public IP of the Jenkins server" # Description of the output
   value       = aws_instance.jenkins_server.public_ip # Value to output (the public IP address)
 }
+
+
+
+#############################################################
+#                                                           #
+#                       VPC creation                        #
+#                                                           #
+#############################################################
+
+# Create a VPC named DCHJ_VPC_Fullstack_CICD
+resource "aws_vpc" "DCHJ_VPC_Fullstack_CICD" {
+  cidr_block           = "10.0.0.0/16"  # IP range for the VPC
+  enable_dns_support   = true  # Enable DNS support within the VPC
+  enable_dns_hostnames = true  # Enable DNS hostnames for instances in the VPC
+
+  # Tags are useful for identifying resources
+  tags = {
+    Name = "DCHJ_VPC_Fullstack_CICD"
+  }
+}
+
+# Create public subnet 01 within the VPC (Can be copied to create another subnet)
+resource "aws_subnet" "DCHJ_Public_Subnet_01" {
+  vpc_id                  = aws_vpc.DCHJ_VPC_Fullstack_CICD.id  # Reference the VPC ID dinamicaly
+  cidr_block              = "10.0.1.0/24"  # IP range for the subnet
+  map_public_ip_on_launch = true  # Automatically assign public IP addresses to instances launched in this subnet
+  availability_zone       = "us-east-1a"  # Change to your desired availability zone
+
+  tags = {
+    Name = "DCHJ_Public_Subnet_01"
+  }
+}
+
+#Create public subnet 02 within the VPC
+resource "aws_subnet" "DCHJ_Public_Subnet_02" {
+  vpc_id                  = aws_vpc.DCHJ_VPC_Fullstack_CICD.id  # Reference the VPC ID dinamicaly
+  cidr_block              = "10.0.2.0/24"  # IP range for the subnet
+  map_public_ip_on_launch = true  # Automatically assign public IP addresses to instances launched in this subnet
+  availability_zone       = "us-east-1b"  # Change to your desired availability zone
+
+  tags = {
+    Name = "DCHJ_Public_Subnet_02"
+  }
+}
+
+/*
+# Create a private subnet within the VPC
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.DCHJ_VPC_Fullstack_CICD.id  # Reference the VPC ID
+  cidr_block        = "10.0.2.0/24"  # IP range for the subnet
+  availability_zone = "us-east-1a"  # Change to your desired availability zone
+
+  tags = {
+    Name = "DCHJ_Private_Subnet"
+  }
+}*/
+
+# Create an internet gateway for the VPC
+resource "aws_internet_gateway" "DCHJ_igw" {
+  vpc_id = aws_vpc.DCHJ_VPC_Fullstack_CICD.id  # Reference the VPC ID
+
+  tags = {
+    Name = "DCHJ_Internet_Gateway"
+  }
+}
+
+# Create a route table for the public subnet
+resource "aws_route_table" "DCHJ_public_rt" {
+  vpc_id = aws_vpc.DCHJ_VPC_Fullstack_CICD.id  # Reference the VPC ID
+
+  # Define a route that sends all traffic (0.0.0.0/0) to the internet gateway
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.DCHJ_igw.id  # Reference the internet gateway ID
+  }
+
+  tags = {
+    Name = "DCHJ_Public_Route_Table"
+  }
+}
+
+# Associate the public subnet 01 with the public route table
+resource "aws_route_table_association" "public_rta_assoc_subnet01" {
+  subnet_id      = aws_subnet.DCHJ_Public_Subnet_01.id  # Reference the public subnet ID
+  route_table_id = aws_route_table.DCHJ_public_rt.id  # Reference the public route table ID
+}
+
+# Associate the public subnet 02 with the public route table
+resource "aws_route_table_association" "public_rt_assoc_subnet02" {
+  subnet_id      = aws_subnet.DCHJ_Public_Subnet_02.id  # Reference the public subnet ID
+  route_table_id = aws_route_table.DCHJ_public_rt.id  # Reference the public route table ID
+}
+/*
+# Create a route table for the private subnet
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.DCHJ_VPC_Fullstack_CICD.id  # Reference the VPC ID
+
+  tags = {
+    Name = "DCHJ_Private_Route_Table"
+  }
+}
+
+# Associate the private subnet with the private route table
+resource "aws_route_table_association" "private_rt_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id  # Reference the private subnet ID
+  route_table_id = aws_route_table.private_rt.id  # Reference the private route table ID
+}
+*/

8cd2c712d1d8b6109b1d5a458da510b634d1d7e4
Cedric-HJ <Devops-cedric.hj@gmail.com>
2024-05-28 09:55:32 +0100

    update to bash scripts

--- Bash/Start_EC2.sh
+++ Bash/Start_EC2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace 'instance_id1', 'instance_id2', 'instance_id3' with your actual instance IDs
-INSTANCE_IDS="Jenkins Ansible"
+INSTANCE_IDS="i-03985fcb9ffdde5d2 i-094c097b1a41e8832"
 
 aws ec2 start-instances --instance-ids $INSTANCE_IDS
 echo "EC2 instances started successfully!"

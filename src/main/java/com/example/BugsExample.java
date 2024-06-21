5177de33465beb6e8ec8b382aca731ed61d83e8c
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-06-21 17:33:24 +0100

    Update BugsExample.java

--- src/main/java/com/example/BugsExample.java
+++ src/main/java/com/example/BugsExample.java
@@ -1,27 +1,17 @@
 public class BugsExample {
 
-  
-
-    // Bug 2: Incorrect method name
     public void printMessage() {
-        System.out.println("This should be 'System.out.println'");
+        System.out.println("Printing a message");
     }
 
-
-    // Bug 6: Undefined variable used
     public void calculate() {
-        int y = 1 + 10;
+        int result = 1 + 10;
+        System.out.println("Calculation result: " + result);
     }
-
-
-
 
     public static void main(String[] args) {
         BugsExample example = new BugsExample();
-        example.printMessage(); // To trigger Bug 2
-        example.calculate(); // To trigger Bug 6
-       
+        example.printMessage();
+        example.calculate();
     }
-
-
 }

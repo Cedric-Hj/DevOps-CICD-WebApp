2771c9f9973ff52013416997793172e60172c5a2
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-06-21 17:40:39 +0100

    Update BugsExample.java

--- src/main/java/com/example/BugsExample.java
+++ src/main/java/com/example/BugsExample.java
@@ -1,5 +1,7 @@
 public class BugsExample {
 
+    private String password = "myHardcodedPassword123"; // Vulnerability: Hardcoded password
+
     public void printMessage() {
         System.out.println("Printing a message");
     }
@@ -7,11 +9,25 @@ public class BugsExample {
     public void calculate() {
         int result = 1 + 10;
         System.out.println("Calculation result: " + result);
+
+        // Bug 1: Division by zero
+        int divideByZero = result / 0;
+        System.out.println("Result of division by zero: " + divideByZero);
     }
 
     public static void main(String[] args) {
         BugsExample example = new BugsExample();
         example.printMessage();
         example.calculate();
+
+        // Bug 2: Incorrect calculation logic
+        int incorrectResult = 1 + 10 * 2; // Should be 21, but will be 31
+        System.out.println("Incorrect calculation result: " + incorrectResult);
+
+        // Bug 3: Unused variable
+        int unusedVariable;
+
+        // Vulnerability: Hardcoded password usage
+        System.out.println("Using hardcoded password: " + example.password);
     }
 }

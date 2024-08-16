a2c22a3244c890d311e2d7680649388babc77d9e
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-08-16 16:28:25 +0100

    Delete Bash directory

--- Bash/Start_EC2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace 'instance_id1', 'instance_id2', 'instance_id3' with your actual instance IDs
-INSTANCE_IDS="i-0020d020c1e148c46 i-0d89c461f5057bd32"
-
-aws ec2 start-instances --instance-ids $INSTANCE_IDS
-echo "EC2 instances started successfully!"

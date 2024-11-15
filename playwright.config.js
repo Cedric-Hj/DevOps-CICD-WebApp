8139ae975ee03f2a231746487e027a1935218e63
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-15 18:06:01 +0000

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   reporter: [['list'], ['junit', { outputFile: 'results.xml' }]],
-  testDir: './Tests',
+  testDir: './Test',
 });

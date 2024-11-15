472d8c282073b4cb7f9e98b692fc7396f1df65b5
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-15 18:03:07 +0000

    Create test.js

---
+++ Test/test.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@playwright/test');
+
+test('Test Ced\'s Webpage', async ({ page }) => {
+    console.log('Running test: Test Ced\'s Webpage');
+    // Navigate to the page
+    await page.goto('http://192.168.0.101:31804');
+    
+    // Check if the heading is correct
+    const heading = await page.locator('h1');
+    await expect(heading).toHaveText('Hello there');
+    
+    // Check if the paragraph text for "dev environment" is correct
+    const paragraph = await page.locator('p:has-text("dev environment")');
+    await expect(paragraph).toHaveText('dev environment');
+    
+    // Check if the version ID is present and has correct text
+    const version = await page.locator('#version');
+    await expect(version).toHaveText('v2.0.3');
+    
+    // Check if the GIF image is visible
+    const gifImage = await page.locator('img[alt="Animated GIF"]');
+    await expect(gifImage).toBeVisible();
+});

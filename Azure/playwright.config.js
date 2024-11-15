cd300e4a43398476c2d415d53cf8c8500490e60c
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-15 16:33:34 +0000

    Update playwright.config.js

--- Azure/playwright.config.js
+++ Azure/playwright.config.js
@@ -2,7 +2,5 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './Azure/tests',  // Ensure this points to the correct directory
-  reporter: [
-    ['junit', { outputFile: 'test-results/test-results.xml' }] // Specify the output directory and file name
-  ],
+  reporter: [['junit', { outputFile: 'results.xml' }]],
 });

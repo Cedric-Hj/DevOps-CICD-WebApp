175eb310122058c99c65a26d205f6b6854ad6991
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-15 17:32:04 +0000

    Update playwright.config.js

--- Azure/playwright.config.js
+++ Azure/playwright.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from '@playwright/test';
 
+console.log('Playwright configuration loaded');
+
 export default defineConfig({
   reporter: [['junit', { outputFile: 'results.xml' }]],
-  testDir: './Azure/tests' // Ensure this points to the correct directory
+  testDir: './Azure/tests', // Ensure this points to the correct directory
 });
-
-

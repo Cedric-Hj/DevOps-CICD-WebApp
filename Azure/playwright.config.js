06c88eb30dc20abf8a780e95ea0d988041e02ed6
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-15 17:55:58 +0000

    Delete Azure/playwright.config.js

--- Azure/playwright.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-
-module.exports = defineConfig({
-  reporter: [['list'], ['junit', { outputFile: 'Azure/results.xml' }]],
-  testDir: './Azure/tests',
-});

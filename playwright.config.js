11975b0e31e9e3439b7420ead06c6c897955f722
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-18 14:45:19 +0000

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require('@playwright/test');
 
-module.exports = defineConfig({
-  reporter: [['list'], ['junit', { outputFile: './test-results/results.xml' }]],
-  testDir: './Test',
-});
+// playwright.config.js
+module.exports = {
+  use: {
+    // Enable video recording for all tests
+    recordVideo: {
+      dir: 'videos/',  // Save videos to the 'videos' folder
+      size: { width: 1280, height: 720 },
+    },
+  },
+  reporter: [
+    ['junit', { outputFile: 'test-results/results.xml' }],  // JUnit report output
+    ['html', { open: 'never' }]  // Optional: HTML report for interactive viewing
+  ],
+};

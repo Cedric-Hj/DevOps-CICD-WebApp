d5c5b32c721f83ef2137253ce4d3d2de067dbccc
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-18 15:40:36 +0000

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,11 @@
-const path = require('path');
+const { defineConfig } = require('@playwright/test');
 
-module.exports = {
+module.exports = defineConfig({
   use: {
-    recordVideo: {
-      dir: '/var/lib/jenkins/workspace/Tests/Playw/playwright-report',  // Absolute path for video recording
-      size: { width: 1280, height: 720 },  // Optional: Set video resolution
-      video: 'on',  // Record video for all tests
-    },
+    video: 'on',
   },
   reporter: [
-    ['junit', { outputFolder: '/var/lib/jenkins/workspace/Tests/Playw/playwright-report', open: 'never' }],
-    ['xml', { outputFolder: '/var/lib/jenkins/workspace/Tests/Playw/playwright-report', open: 'never' }],
-    ['html', { outputFolder: '/var/lib/jenkins/workspace/Tests/Playw/playwright-report', open: 'never' }],
+    ['list'],
+    ['./xmlReporter.js']
   ],
-};
+});
